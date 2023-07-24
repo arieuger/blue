88e302190243ddf1d73e675441c5e3b2bcf50d37
A. Roig <antiaroig@gmail.com>
2023-07-25 01:41:35 +0200

    CharacterController

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Vector3 moveDirection;
+    private bool pressedJumpButton;
+    
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Movement(Vector3 direction) 
+    {
+        // TODO: Revisar movemento con inputactions
+        
+        if (controller.isGrounded) 
+        {
+            moveDirection = direction;
+            moveDirection *= speed;
+            
+            if (pressedJumpButton)
+            {
+                moveDirection.y = jumpHeight;
+                pressedJumpButton = false;
+            }
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    public void Jump() {
+        pressedJumpButton = true;
+    }
+}
---
+++ Assets/Scripts/PlayerInputReader.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    
+    private GameInputs input;
+    private Player player;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+        input = new GameInputs();
+        input.Player.Enable();
+        input.Player.Jump.performed += JumpPerformed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vInput = input.Player.Movement.ReadValue<Vector2>();
+        Vector3 direction = new Vector3(vInput.x, 0, 0);
+        player.Movement(direction);
+    }
+
+    private void JumpPerformed(InputAction.CallbackContext context)
+    {
+        player.Jump();
+    }
+}
